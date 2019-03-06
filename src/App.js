38fc5dd1fa1f628c8bd75b3c8b52194f3179e3af
bergurijohansen <bergurij@gmail.com>
2019-03-06 14:07:47 +0000

    jasf

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+import Dino from "./dinosaur/dinosaur";
+class App extends Component {
+  render() {
+    return <Dino />;
+  }
+}
+
+export default App;
